9a2a7e451752067710b4bd10e1c2ccfb5ec51299
Jakub Plesnik <jakub.plesnik@sde.cz>
2019-08-01 09:50:07 +0200

    Update memory management

--- DependencyTransformation/Program.cs
+++ DependencyTransformation/Program.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,7 +31,7 @@ namespace DependencyTransformation
             }
 
             Console.WriteLine("Start of experiment");
-            for (int i = 0; i < 1; i++ )
+            for (int i = 0; i < 5; i++ )
             {
                 Console.WriteLine("Start of the round {0}",i);
                 sw = new Stopwatch();
@@ -43,6 +43,11 @@ namespace DependencyTransformation
                 sw.Stop();
                 time[0] += sw.ElapsedTicks;
                 sw.Reset();
+                dc = null;
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
 
                 dc = new DependencyCalculator();
                 sw.Start();
@@ -52,6 +57,10 @@ namespace DependencyTransformation
                 sw.Stop();
                 time[1] += sw.ElapsedTicks;
                 sw.Reset();
+                dc = null;
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
 
                 dc = new DependencyCalculator();
                 sw.Start();
@@ -61,6 +70,10 @@ namespace DependencyTransformation
                 sw.Stop();
                 time[2] += sw.ElapsedTicks;
                 sw.Reset();
+                dc = null;
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
 
                 dc = new DependencyCalculator();
                 sw.Start();
@@ -70,23 +83,27 @@ namespace DependencyTransformation
                 sw.Stop();
                 time[3] += sw.ElapsedTicks;
                 sw.Reset();
+                dc = null;
 
-                /*
-                dc = new DependencyCalculator();
-                sw.Start();
-                dc.LoadData(GraphPath);
-                dc.ParallelNaiveThreadTransformation();
-                result = dc.getDependencyMatrix();
-                sw.Stop();
-                time[4] += sw.ElapsedTicks;
-                sw.Reset();*/
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+
+                /* dc = new DependencyCalculator();
+                 sw.Start();
+                 dc.LoadData(GraphPath);
+                 dc.ParallelNaiveThreadTransformation();
+                 result = dc.getDependencyMatrix();
+                 sw.Stop();
+                 time[4] += sw.ElapsedTicks;
+                 sw.Reset();*/
             }
 
             Console.WriteLine("Method: Sequential; Ticks: {0}", time[0]/5);
             Console.WriteLine("Method: Tasks; Ticks: {0}", time[1]/5);
             Console.WriteLine("Method: Parallel Manager; Ticks: {0}", time[2]/5);
             Console.WriteLine("Method: Native Parallel For; Ticks: {0}", time[3]/5);
-           // Console.WriteLine("Method: Naive Parallel; Ticks: {0} ms", time[4]/5);
+            //Console.WriteLine("Method: Naive Parallel; Ticks: {0} ms", time[4]/5);
         }  
     }
 }
